0170246568942cb2f0e5a5820fd2e9689cd9d9dc
KIRA <kanavraina98@gmail.com>
2020-01-14 12:59:07 +0530

    Create variables.sol

---
+++ variables.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+ 
+contract VariableExamples{
+    bool public switchedOn = true;
+    address public owner = msg.sender;
+    uint8 public number = 8;
+    bytes32 public awesome1 = "Solidity is awesome!";
+    string public awesome2 = "Solidity is awesome!";
+}
