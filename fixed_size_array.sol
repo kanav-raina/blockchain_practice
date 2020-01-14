ebc0aa8faa4473e78a7cf07cc69283ab8f955b27
KIRA <kanavraina98@gmail.com>
2020-01-14 13:05:05 +0530

    Create fixed_size_array.sol

---
+++ fixed_size_array.sol
@@ -0,0 +1,16 @@
+pragma solidity^0.4.24;
+ 
+contract SampleContract {
+    uint8[3] nums = [10, 20, 30];
+ 
+    function getNums() public returns (uint8[3]){
+        nums[0] = 11;
+        nums[1] = 22;
+        nums[2] = 33;
+        return nums;
+    }
+    
+    function getLength() view public returns (uint){
+        return nums.length;
+    }
+}
